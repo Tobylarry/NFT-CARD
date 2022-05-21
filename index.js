f8f7a61b141eac102d18ce61ff42c117be94b3d1
Tobi <47112773+Tobylarry@users.noreply.github.com>
2022-05-21 16:09:28 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+
+let profileName = document.querySelector('.profileName');
+let creation = document.querySelector('.creation');
+
+function create(x){
+    profileName.style.color = "hsl(178, 100%, 50%)"
+    creation.style.color = "white"
+}
